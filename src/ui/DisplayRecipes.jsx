8703916e98a97611b1cd858ce5295af2b8885b85
Milena Oswald <milena.oswald@icloud.com>
2024-03-13 17:23:26 +0100

    DisplayREcipe komponente update caption und teaser

--- src/ui/DisplayRecipes.jsx
+++ src/ui/DisplayRecipes.jsx
@@ -1,8 +1,9 @@
 function DisplayRecipes({recipe}) {
     return (
         <div className='recipe-card'>
-            <img className="recipe-image"src={recipe.fields.image.fields.file.url} alt={recipe.fields.title} />
             <figcaption className="recipe-caption">{recipe.fields.title}</figcaption>
+            <img className="recipe-image"src={recipe.fields.image.fields.file.url} alt={recipe.fields.title} />
+            <div className="recipe-teaser">{recipe.fields.image.fields.description}</div>
         </div>
     );
 }
